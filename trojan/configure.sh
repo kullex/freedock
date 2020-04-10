e5f0db590a4e52073534554b496e4966e2cd5b40
kullex <kullex@tutanota.com>
2020-04-10 20:50:26 +0800

    trojan finished

--- trojan/configure.sh
+++ trojan/configure.sh
@@ -2,31 +2,6 @@
 
 set -e
 
-# download and install latest trojan
-
-NAME=trojan
-VERSION=$(curl -fsSL https://api.github.com/repos/trojan-gfw/trojan/releases/latest | grep tag_name | sed -E 's/.*"v(.*)".*/\1/')
-TARBALL="$NAME-$VERSION-linux-amd64.tar.xz"
-DOWNLOADURL="https://github.com/trojan-gfw/$NAME/releases/download/v$VERSION/$TARBALL"
-TMPDIR="$(mktemp -d)"
-INSTALLPREFIX=/usr/local
-SYSTEMDPREFIX=/etc/systemd/system
-
-BINARYPATH="$INSTALLPREFIX/bin/$NAME"
-
-echo Entering temp directory $TMPDIR...
-cd "$TMPDIR"
-
-echo Downloading $NAME $VERSION...
-curl -LO --progress-bar "$DOWNLOADURL" || wget -q --show-progress "$DOWNLOADURL"
-
-echo Unpacking $NAME $VERSION...
-tar xf "$TARBALL"
-cd "$NAME"
-
-echo Installing $NAME $VERSION to $BINARYPATH...
-install -Dm755 "$NAME" "$BINARYPATH"
-
 # waiting for ssl
 
 until [ `ls /ssl|wc -w` -eq 1 ]
@@ -35,4 +10,7 @@ do
   sleep 1
 done
 
-exec $@
+ls /ssl
+
+echo "starting trojan..."
+/usr/local/bin/trojan /config/config.json
