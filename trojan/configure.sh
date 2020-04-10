77d7197ea95e1a6f5502b5f780dda4bdcf3eefd6
kullex <kullex@tutanota.com>
2020-04-10 16:47:56 +0800

    using prebuild trojan

--- trojan/configure.sh
+++ trojan/configure.sh
@@ -2,6 +2,33 @@
 
 set -e
 
+# download and install latest trojan
+
+NAME=trojan
+VERSION=$(curl -fsSL https://api.github.com/repos/trojan-gfw/trojan/releases/latest | grep tag_name | sed -E 's/.*"v(.*)".*/\1/')
+TARBALL="$NAME-$VERSION-linux-amd64.tar.xz"
+DOWNLOADURL="https://github.com/trojan-gfw/$NAME/releases/download/v$VERSION/$TARBALL"
+TMPDIR="$(mktemp -d)"
+INSTALLPREFIX=/usr/local
+SYSTEMDPREFIX=/etc/systemd/system
+
+BINARYPATH="$INSTALLPREFIX/bin/$NAME"
+
+echo Entering temp directory $TMPDIR...
+cd "$TMPDIR"
+
+echo Downloading $NAME $VERSION...
+curl -LO --progress-bar "$DOWNLOADURL" || wget -q --show-progress "$DOWNLOADURL"
+
+echo Unpacking $NAME $VERSION...
+tar xf "$TARBALL"
+cd "$NAME"
+
+echo Installing $NAME $VERSION to $BINARYPATH...
+install -Dm755 "$NAME" "$BINARYPATH"
+
+# waiting for ssl
+
 until [ `ls -A /ssl|wc -w` -eq 1 ]
 do
   >&2 echo "waiting for ssl certs..."
